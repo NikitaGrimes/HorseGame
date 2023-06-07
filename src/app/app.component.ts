dff462a855b7112f13c8c236cf98fc2e07919440
Довнар Никита Сергеевич <dounar@sc.softclub.by>
2023-06-07 18:19:40 +0300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameCell } from './game-cell';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'horse-game';
+export class AppComponent implements OnInit{
+  cellNumber: number = 9;
+  gameBoardSize: number = 700;
+  cells: GameCell[][] = [];
+  
+  ngOnInit(): void {
+    this.cells = new Array<GameCell[]>(this.cellNumber).fill(new Array<GameCell>(this.cellNumber)).map((line, i) => line.map((cell, j) => new GameCell(i, j)));
+  }
+
+  onClick(line: number, cell: number):void{
+    console.log(line + " " + cell);
+  }
 }
---
+++ src/app/game-cell.ts
@@ -0,0 +1,5 @@
+export class GameCell {
+    constructor(public lineNumber: number, public cellNumber: number, public isChecked: boolean = false){
+
+    }
+}

fd565002b64d08c3e5d3a132c69d7ac8ecb08393
Довнар Никита Сергеевич <dounar@sc.softclub.by>
2023-06-08 11:24:59 +0300

    working base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { GameCell } from './game-cell';
 
 @Component({
@@ -6,16 +6,64 @@ import { GameCell } from './game-cell';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   cellNumber: number = 9;
   gameBoardSize: number = 700;
   cells: GameCell[][] = [];
-  
-  ngOnInit(): void {
-    this.cells = new Array<GameCell[]>(this.cellNumber).fill(new Array<GameCell>(this.cellNumber)).map((line, i) => line.map((cell, j) => new GameCell(i, j)));
+  moveableCells: GameCell[] = [];
+  prevMove?: GameCell = undefined;
+  currentMove?: GameCell = undefined;
+
+  constructor(){
+    this.cells = this.initialGameBoard();
   }
 
-  onClick(line: number, cell: number):void{
-    console.log(line + " " + cell);
+  logging(cell: GameCell): void{
+    console.log(cell);
+  }
+
+  initialGameBoard(): GameCell[][]{
+    return new Array<GameCell[]>(this.cellNumber)
+    .fill(new Array<GameCell>(this.cellNumber)
+      .fill(new GameCell(0, 0)))
+    .map((line, i) => line.map((cell, j) => new GameCell(i, j)));
+  }
+
+  setFirstMove(): void{
+    if(!this.currentMove)
+      this.cells.forEach(lines => lines.forEach(cell => cell.setDefault()));
+  }
+
+  setMoveableCells(currentCell: GameCell): void{
+    this.cells.forEach(lines => lines.forEach(cell => {
+      if (!cell.isChecked && cell.checkMoveable(currentCell.lineNumber, currentCell.cellNumber)) {
+        cell.setMoveable();
+        this.moveableCells.push(cell);
+      }
+    }));
+  }
+
+  clearMoveableCells(): void{
+    this.moveableCells.forEach(cell => cell.setDefault());
+    this.moveableCells = [];
+  }
+
+  makingMove(cell: GameCell): void{
+    this.prevMove = this.currentMove;
+    this.currentMove = cell;
+    cell.setCurrent();
+    if(this.prevMove)
+      this.prevMove.setCheacked();
+  }
+
+  onClick(newcurrentCell: GameCell):void{
+    this.logging(newcurrentCell);
+    if(!newcurrentCell.isCanMove)
+      return;
+
+    this.setFirstMove();
+    this.clearMoveableCells();
+    this.makingMove(newcurrentCell);
+    this.setMoveableCells(newcurrentCell);
   }
 }
--- src/app/game-cell.ts
+++ src/app/game-cell.ts
@@ -1,5 +1,48 @@
 export class GameCell {
-    constructor(public lineNumber: number, public cellNumber: number, public isChecked: boolean = false){
+    private static readonly _moves: Array<readonly [number, number]> = [
+        [+1, +2],
+        [+2, +1],
+        [-1, +2],
+        [-2, +1],
+        [+1, -2],
+        [+2, -1],
+        [-1, -2],
+        [-2, -1],
+    ]
+
+    private static _movesNumber: number = 0;
+
+    constructor(public lineNumber: number, public cellNumber: number, public isChecked: boolean = false, 
+        public isCanMove: boolean = true, public isCurrent: boolean = false, public moveNumber?: number){
 
     }
+
+    setCheacked(): void{
+        this.isChecked = true;
+        this.isCanMove = this.isCurrent = false;
+    }
+
+    setMoveable(): void{
+        this.isCanMove = true;
+        this.isChecked = this.isCurrent = false;
+    }
+
+    setCurrent(): void{
+        this.isCurrent = true;
+        this.isChecked = this.isCanMove = false;
+        GameCell._movesNumber++;
+        this.moveNumber = GameCell._movesNumber;
+    }
+
+    setDefault(): void{
+        this.isCurrent = this.isCanMove = this.isChecked = false;
+    }
+
+    checkMoveable(line: number, cell: number): boolean{
+        return GameCell._moves
+            .map(move => [move[0] + this.lineNumber, move[1] + this.cellNumber])
+            .some(move => move[0] === line && move[1] === cell);
+    }
+
+
 }
